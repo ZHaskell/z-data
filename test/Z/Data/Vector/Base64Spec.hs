b04979b673bdc350f937a89dff89501d576250b9
Han Dong <winterland1989@gmail.com>
2020-11-30 15:33:50 +0800

    upd tests

--- test/Z/Data/Vector/Base64Spec.hs
+++ test/Z/Data/Vector/Base64Spec.hs
@@ -10,7 +10,7 @@ import           Test.Hspec
 import           Test.Hspec.QuickCheck
 
 spec :: Spec
-spec = describe "text-Base64Bytes" . modifyMaxSuccess (*50) . modifyMaxSize (*50) $ do
+spec = describe "text-Base64Bytes" . modifyMaxSuccess (*10) . modifyMaxSize (*10) $ do
     describe "encoding props " $ do
         prop "base64Decode' . base64Encode === id" $ \ xs ->
             (B.base64Decode' (B.base64Encode xs) === xs)
