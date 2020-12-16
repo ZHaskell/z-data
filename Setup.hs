61e0d6ab22dd6a4ce840721c23bbe4f8956c61c6
Dong Han <dong.han@emqx.io>
2020-12-16 14:29:24 +0800

    add -optcxx-std=c++11 when GHC >= 8.10

--- Setup.hs
+++ Setup.hs
@@ -1,9 +1,14 @@
+{-# LANGUAGE CPP #-}
 import           Distribution.Simple
 import           System.Environment
 
 main = do
+#if __GLASGOW_HASKELL__ < 810
+  defaultMain
+#else
   args <- getArgs
   if head args == "configure"
      then defaultMainArgs $ [ "--ghc-options", "-optcxx-std=c++11"
                             ] ++ args
      else defaultMain
+#endif
