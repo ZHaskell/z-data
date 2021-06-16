a3ec25a41a8622d84297b7849c696a2229b1482a
Dong Han <winterland1989@gmail.com>
2021-06-16 18:16:29 +0800

    -cxx-isystem

--- Setup.hs
+++ Setup.hs
@@ -5,35 +5,32 @@ import           Distribution.Simple.Utils
 import           Distribution.Simple.Program
 import           System.Environment
 import qualified System.Environment as System
+import           Distribution.Verbosity
 
 main = do
-    mainArgs <- getArgs
-    if head mainArgs == "build"
 #ifdef darwin_HOST_OS
-    then defaultMainWithHooksArgs simpleUserHooks
-            { preBuild = \ a b -> getSysroot a b >> preBuild simpleUserHooks a b }
-#else
-    then defaultMainWithHooksArgs simpleUserHooks
-#endif
-
-#if __GLASGOW_HASKELL__ < 810
-            mainArgs
-#else
-            (if cabalVersion <= mkVersion [3,2,0] then ("--ghc-options":"-optcxx-std=c++11":mainArgs) else mainArgs)
-#endif
-
-    else defaultMain
-
-
-getSysroot :: Args -> BuildFlags -> IO ()
-getSysroot _ flags = do
-    let verbosity = fromFlag $ buildVerbosity flags
-    sysroot <- getProgramInvocationOutput verbosity (simpleProgramInvocation "xcrun" ["--show-sdk-path"])
+    sysroot <- getProgramInvocationOutput normal (simpleProgramInvocation "xcrun" ["--show-sdk-path"])
     let sysroot' = head (lines sysroot)
         system = sysroot' ++ "/../../usr/include"
-    notice verbosity ("Use sysroot at: " ++ sysroot')
-    notice verbosity ("Use system include at: " ++ system)
-    cflags <- lookupEnv "CFLAGS" >>= return . maybe "" id
-    setEnv "CFLAGS" $ "-isysroot " ++ sysroot' ++ " -isystem " ++ system ++ (' ' : cflags)
-    cxxflags <- lookupEnv "CXXFLAGS" >>= return . maybe "" id
-    setEnv "CXXFLAGS" $ "-isysroot " ++ sysroot' ++ " -cxx-isystem " ++ system ++ (' ' : cxxflags)
+#endif
+    mainArgs <- getArgs
+    if head mainArgs == "build"
+    then do
+        let mainArgs' =
+#ifdef darwin_HOST_OS
+                "--ghc-options":("-optcxx-cxx-isystem"++ system):mainArgs
+#else
+                mainArgs
+#endif
+
+        let mainArgs'' =
+#if __GLASGOW_HASKELL__ < 810
+                mainArgs'
+#else
+                if cabalVersion <= mkVersion [3,2,0]
+                then ("--ghc-options":"-optcxx-std=c++11":mainArgs')
+                else mainArgs'
+#endif
+
+        defaultMainWithHooksArgs simpleUserHooks mainArgs''
+    else defaultMain
